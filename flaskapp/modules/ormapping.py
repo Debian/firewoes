22241f625fb9f1010f02e773473eb2c1cb97b0eb
Matthieu Caneill <matthieu.caneill42@gmail.com>
2013-05-20 17:03:32 +0200

    indentation

--- flaskapp/modules/ormapping.py
+++ flaskapp/modules/ormapping.py
@@ -18,16 +18,17 @@ from firehose_noslots import *
 
 t_analysis = \
     Table('analysis', metadata,
-             Column('id', Integer, primary_key=True),
-             Column('metadata_id', Integer,
-                       ForeignKey('metadata.id'), nullable=False),
-             Column('customfields_id', Integer,
-                       ForeignKey('customfields.id')),
-             )
+          Column('id', Integer, primary_key=True),
+          Column('metadata_id', Integer,
+                 ForeignKey('metadata.id'), nullable=False),
+          Column('customfields_id', Integer,
+                 ForeignKey('customfields.id')),
+          )
 
 t_generator = \
     Table('generator', metadata,
-          Column('id', Integer, Sequence('foo', start=1, increment=1), unique=True),
+          Column('id', Integer, Sequence('foo', start=1, increment=1),
+                 unique=True),
           Column('name', String, nullable=False, primary_key=True),
           Column('version', String, primary_key=True),
           )
@@ -36,7 +37,7 @@ t_metadata = \
     Table('metadata', metadata,
           Column('id', Integer, primary_key=True),
           Column('generator_id', Integer,
-                    ForeignKey('generator.id'), nullable=False),
+                 ForeignKey('generator.id'), nullable=False),
           Column('sut_id', Integer, ForeignKey('sut.id')),
           Column('file_id', Integer, ForeignKey('file.id')),
           Column('stats_id', Integer, ForeignKey('stats.id')),
@@ -44,177 +45,178 @@ t_metadata = \
 
 t_stats = \
     Table('stats', metadata,
-             Column('id', Integer, primary_key=True),
-             Column('wallclocktime', Float, nullable=False),
-             )
+          Column('id', Integer, primary_key=True),
+          Column('wallclocktime', Float, nullable=False),
+          )
 
 # For the Sut hierarchy we use single-table inheritance
 t_sut = \
     Table('sut', metadata,
-             Column('id', Integer, primary_key=True),
-             Column('type', String(20), nullable=False),
-             Column('name', String, nullable=False),
-             Column('version', String, nullable=False),
-             Column('release', String),
-             Column('buildarch', String),
-             )
+          Column('id', Integer, primary_key=True),
+          Column('type', String(20), nullable=False),
+          Column('name', String, nullable=False),
+          Column('version', String, nullable=False),
+          Column('release', String),
+          Column('buildarch', String),
+          )
+# TODOOOOOOOOOOOOO
 
 # For the Result hierarchy we use joined-table inheritance
 t_result = \
     Table('result', metadata,
-             Column('id', Integer, primary_key=True), # renamed from result_id
-             Column('analysis_id', Integer,
-                       ForeignKey('analysis.id'), nullable=False),
-             Column('type', String(10), nullable=False),
-             )
+          Column('id', Integer, primary_key=True), # renamed from result_id
+          Column('analysis_id', Integer,
+                 ForeignKey('analysis.id'), nullable=False),
+          Column('type', String(10), nullable=False),
+          )
 
 t_issue = \
     Table('issue', metadata,
-             Column('result_id', Integer,
-                       ForeignKey('result.id'), primary_key=True),
-             Column('cwe', Integer),
-             Column('testid', String),
-             Column('location_id', Integer,
-                       ForeignKey('location.id'), nullable=False),
-             Column('message_id', Integer,
-                       ForeignKey('message.id'), nullable=False),
-             Column('notes_id', Integer, ForeignKey('notes.id')),
-             Column('trace_id', Integer, ForeignKey('trace.id')),
-             Column('severity', String),
-             Column('customfields_id', Integer,
-                       ForeignKey('customfields.id')),
-             )
+          Column('result_id', Integer,
+                 ForeignKey('result.id'), primary_key=True),
+          Column('cwe', Integer),
+          Column('testid', String),
+          Column('location_id', Integer,
+                 ForeignKey('location.id'), nullable=False),
+          Column('message_id', Integer,
+                 ForeignKey('message.id'), nullable=False),
+          Column('notes_id', Integer, ForeignKey('notes.id')),
+          Column('trace_id', Integer, ForeignKey('trace.id')),
+          Column('severity', String),
+          Column('customfields_id', Integer,
+                 ForeignKey('customfields.id')),
+          )
 
 t_failure = \
     Table('failure', metadata,
-             Column('result_id', Integer,
-                       ForeignKey('result.id'),
-                       primary_key=True),
-             Column('failureid', String),
-             Column('location_id', Integer,
-                       ForeignKey('location.id')),
-             Column('message_id', Integer,
-                       ForeignKey('message.id')),
-             Column('customfields_id', Integer,
-                       ForeignKey('customfields.id')),
-             )
+          Column('result_id', Integer,
+                 ForeignKey('result.id'),
+                 primary_key=True),
+          Column('failureid', String),
+          Column('location_id', Integer,
+                 ForeignKey('location.id')),
+          Column('message_id', Integer,
+                 ForeignKey('message.id')),
+          Column('customfields_id', Integer,
+                 ForeignKey('customfields.id')),
+          )
 
 t_info = \
     Table('info', metadata,
-             Column('result_id', Integer,
-                       ForeignKey('result.id'), primary_key=True),
-             Column('infoid', String),
-             Column('location_id', Integer,
-                       ForeignKey('location.id')),
-             Column('message_id', Integer,
-                       ForeignKey('message.id')),
-             Column('customfields_id', Integer,
-                       ForeignKey('customfields.id')),
+          Column('result_id', Integer,
+                 ForeignKey('result.id'), primary_key=True),
+          Column('infoid', String),
+          Column('location_id', Integer,
+                 ForeignKey('location.id')),
+          Column('message_id', Integer,
+                 ForeignKey('message.id')),
+          Column('customfields_id', Integer,
+                 ForeignKey('customfields.id')),
           )
 
 t_message = \
     Table('message', metadata,
-             Column('id', Integer, primary_key=True),
-             Column('text', String, nullable=False),
-             )
+          Column('id', Integer, primary_key=True),
+          Column('text', String, nullable=False),
+          )
 # ideally we should just store a String where these get used
 
 t_notes = \
     Table('notes', metadata,
-             Column('id', Integer, primary_key=True),
-             Column('text', String, nullable=False),
-             )
+          Column('id', Integer, primary_key=True),
+          Column('text', String, nullable=False),
+          )
 # ideally we should just store a String where these get used
 
 
 t_trace = \
     Table('trace', metadata,
-             Column('id', Integer, primary_key=True)
-             )
+          Column('id', Integer, primary_key=True)
+          )
 
 t_state = \
     Table('state', metadata,
-             Column('id', Integer, primary_key=True),
-             Column('trace_id', Integer,
-                       ForeignKey('trace.id')),
-             Column('location_id', Integer,
-                       ForeignKey('location.id'), nullable=False),
-             Column('notes_id', Integer,
-                       ForeignKey('notes.id')),
-             )
+          Column('id', Integer, primary_key=True),
+          Column('trace_id', Integer,
+                 ForeignKey('trace.id')),
+          Column('location_id', Integer,
+                 ForeignKey('location.id'), nullable=False),
+          Column('notes_id', Integer,
+                 ForeignKey('notes.id')),
+          )
 
 t_location = \
     Table('location', metadata,
-             Column('id', Integer, primary_key=True),
-             Column('file_id', Integer,
-                       ForeignKey('file.id'), nullable=False),
-             Column('function_id', Integer,
-                       ForeignKey('function.id')),
-             Column('point_id', Integer,
-                       ForeignKey('point.id')),
-             Column('range_id', Integer,
-                       ForeignKey('range.id')),
-             )
+          Column('id', Integer, primary_key=True),
+          Column('file_id', Integer,
+                 ForeignKey('file.id'), nullable=False),
+          Column('function_id', Integer,
+                 ForeignKey('function.id')),
+          Column('point_id', Integer,
+                 ForeignKey('point.id')),
+          Column('range_id', Integer,
+                 ForeignKey('range.id')),
+          )
 
 t_file = \
     Table('file', metadata,
-             Column('id', Integer, primary_key=True),
-             Column('givenpath', String, nullable=False),
-             Column('abspath', String),
-             Column('hash_id', Integer, ForeignKey('hash.id')),
-             )
+          Column('id', Integer, primary_key=True),
+          Column('givenpath', String, nullable=False),
+          Column('abspath', String),
+          Column('hash_id', Integer, ForeignKey('hash.id')),
+          )
 
 t_hash = \
     Table('hash', metadata,
-             Column('id', Integer, primary_key=True),
-             Column('alg', String, nullable=False),
-             Column('hexdigest', String, nullable=False),
-             )
+          Column('id', Integer, primary_key=True),
+          Column('alg', String, nullable=False),
+          Column('hexdigest', String, nullable=False),
+          )
 
 t_function = \
     Table('function', metadata,
-             Column('id', Integer, primary_key=True),
-             Column('name', String, nullable=False),
-                  # in firehose: can be None
-             )
+          Column('id', Integer, primary_key=True),
+          Column('name', String, nullable=False),
+            # in firehose: can be None
+          )
 
 t_point = \
     Table('point', metadata,
-             Column('id', Integer, primary_key=True),
-             Column('line', Integer, nullable=False),
-             Column('column', Integer, nullable=False),
-             )
+          Column('id', Integer, primary_key=True),
+          Column('line', Integer, nullable=False),
+          Column('column', Integer, nullable=False),
+          )
 
 t_range = \
     Table('range', metadata,
-             Column('id', Integer, primary_key=True),
-             Column('start_id', Integer,
-                       ForeignKey('point.id'), nullable=False),
-             Column('end_id', Integer,
-                       ForeignKey('point.id'), nullable=False),
-             )
+          Column('id', Integer, primary_key=True),
+          Column('start_id', Integer,
+                 ForeignKey('point.id'), nullable=False),
+          Column('end_id', Integer,
+                 ForeignKey('point.id'), nullable=False),
+          )
 
 t_customfields = \
     Table('customfields', metadata,
-             Column('id', Integer, primary_key=True))
+          Column('id', Integer, primary_key=True))
 
 t_intfield = \
     Table('intfield', metadata,
-             Column('id', Integer, primary_key=True),
-             Column('customfields_id', Integer,
-                       ForeignKey('customfields.id'), nullable=False),
-             Column('name', String, nullable=False),
-             Column('value', Integer, nullable=False),
-             )
+          Column('id', Integer, primary_key=True),
+          Column('customfields_id', Integer,
+                 ForeignKey('customfields.id'), nullable=False),
+          Column('name', String, nullable=False),
+          Column('value', Integer, nullable=False),
+          )
 
 t_strfield = \
     Table('strfield', metadata,
-             Column('id', Integer, primary_key=True),
-             Column('customfields_id', Integer,
-                       ForeignKey('customfields.id'), nullable=False),
-             Column('name', String, nullable=False),
-             Column('value', String, nullable=False),
-             )
+          Column('id', Integer, primary_key=True),
+          Column('customfields_id', Integer,
+                 ForeignKey('customfields.id'), nullable=False),
+          Column('name', String, nullable=False),
+          Column('value', String, nullable=False),
+          )
 
 ############################################################################
 # Mappers
