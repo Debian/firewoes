db66f12d7bb3706db14fa1027fa5f3b11767b054
Matthieu Caneill <matthieu.caneill42@gmail.com>
2013-06-04 18:22:09 +0200

    generate_fake_base: import corrections, add output_dir argument

--- bin/generate_fake_base.py
+++ bin/generate_fake_base.py
@@ -1,5 +1,7 @@
 # generates fake analyses into xml files
 
+import os
+import argparse
 import random
 import subprocess
 from firehose.model import Analysis, Metadata, Generator, Issue, \
@@ -7,7 +9,8 @@ from firehose.model import Analysis, Metadata, Generator, Issue, \
 
 
 analysis_tools = [("coccinelle", "1.0"),
-                  ("coccinelle", "spatch version 1.0.0-rc15 with Python support and with PCRE support"),
+                  ("coccinelle", "spatch version 1.0.0-rc15 with Python "
+                   "support and with PCRE support"),
                   ("clang", "1.2.3"),
                   ("cool-analysis-tool", "0.1")]
 
@@ -23,7 +26,7 @@ functions = [("hello.c", "hello_world()"),
              ("run.py", "generate_test(var=8)"),
              ("foo.cpp", "string_from_int(integer)")]
 
-if __name__ == "__main__":
+def generate_fake_bases(output_dir):
     analyses = []
     for tool, tool_version in analysis_tools:
         for sut, sut_version in software:
@@ -36,7 +39,12 @@ if __name__ == "__main__":
                 file_ = File(file_, None, None)
                 function = Function(function)
                 location = Location(file_, function)
-                fortune = subprocess.check_output(["fortune"])
+                try:
+                    fortune = subprocess.check_output(["fortune"])
+                except OSError:
+                    print("'fortune' is not installed and required to"
+                          "generate fake messages")
+                    import sys; sys.exit()
                 message = Message(fortune)
                 issue = Issue(None, None, location, message, None, None)
                 issues.append(issue)
@@ -44,7 +52,18 @@ if __name__ == "__main__":
             analyses.append(analysis)
     i = 0
     for analysis in analyses:
-        f = open("analysis" + str(i) + ".xml", "w")
+        f = open(os.path.join(output_dir, "analysis" + str(i) + ".xml"), "w")
         f.write(analysis.to_xml_bytes())
         f.close()
         i += 1
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Generates random Firehose "
+                                     "analysis*.xml files")
+    parser.add_argument("output_dir",
+                        help="folder where to save the outputted xml files")
+    args = parser.parse_args()
+    
+    generate_fake_bases(args.output_dir)
+    
