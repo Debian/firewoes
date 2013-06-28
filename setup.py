59360a105c779632e6e45787ed73e9176baadde1
Matthieu Caneill <matthieu.caneill42@gmail.com>
2013-06-28 11:09:55 +0200

    setup: add __appname__ and __version__ in __init__

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
+from firewose import __appname__, __version__
 
 with open('requirements.txt') as f:
     install_requires = [l for l in f.read().splitlines()
@@ -10,8 +11,8 @@ with open('README.txt') as f:
     long_description = f.read()
 
 setup(
-    name="firewose",
-    version="0.1",
+    name=__appname__,
+    version=__version__,
     packages=find_packages(),
     include_package_data=True,
     install_requires=install_requires,
