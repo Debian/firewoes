80a02e38a2f022f04577f6283df8fee7fa075ff5
Matthieu Caneill <matthieu.caneill42@gmail.com>
2013-07-17 14:21:21 +0200

    add: real cooler footer

--- config/webconfig_local.example.py
+++ config/webconfig_local.example.py
@@ -11,3 +11,6 @@ DEBIAN_SOURCES_URL = "http://sources.debian.net/src/{package}/{version}-{release
 
 # the url pattern used for embedded source code
 DEBIAN_EMBEDDED_SOURCES_URL = "http://sources.debian.net/embedded/{package}/{version}-{release}/{path}?msg={message}&hl={lines_range}#L{anchor}"
+
+# where one can find the source code of the app
+GITWEB_URL = "http://git.upsilon.cc/?p=firewose.git"
