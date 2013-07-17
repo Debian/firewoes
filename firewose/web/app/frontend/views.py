c344ecfe9a998b6f95add278638c86f690870bb8
Matthieu Caneill <matthieu.caneill42@gmail.com>
2013-07-17 12:52:28 +0200

    remove forms.py, useless now

--- firewose/web/app/frontend/views.py
+++ firewose/web/app/frontend/views.py
@@ -26,7 +26,6 @@ from models import Generator_app, Analysis_app, Sut_app, Result_app
 from models import Report
 from models import Http404Error, Http500Error
 from debianutils import get_source_url
-#from forms import SearchForm
 
 # Theme configuration
 theme = app.config["THEME"]
