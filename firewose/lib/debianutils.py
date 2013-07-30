661a330be8cf64a269d0704a490a0c6da7c43ba7
Matthieu Caneill <matthieu.caneill42@gmail.com>
2013-07-30 10:56:06 +0200

    add: debianutils: functions for filtering by maintainer

--- firewose/lib/debianutils.py
+++ firewose/lib/debianutils.py
@@ -27,6 +27,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from debian.debian_support import version_compare
 
 from firewose.lib.orm import *
+from firewose.web.app import session
 from sqlalchemy import and_
 
 """
@@ -100,6 +101,26 @@ def packages_for_person(session, login):
                .all())
     return res
 
+def email_for_person(person):
+    """
+    Given a login, a full name, or an email, returns the corresponding email.
+    """
+    if "@" in person:
+        return person
+    res = (session.query(DebianMaintainer)
+           .filter(DebianMaintainer.name == person)
+           .first())
+    if res:
+        return res.email
+    
+    res = (session.query(DebianMaintainer)
+           .filter(DebianMaintainer.email == person + "@debian.org")
+           .first())
+    if res:
+        return res.email
+    
+    return None
+
 ###################################################
 ### Debian specific tables, objects and mappers ###
 ###################################################
@@ -109,36 +130,32 @@ Base = declarative_base()
 class DebianPackage(Base):
     __tablename__ = "debian_packages"
     
-    name = Column(String, primary_key=True)
+    name = Column(String, primary_key=True, index=True)
+    mappings = relationship("DebianPackagePeopleMapping", backref="package")
     
     def __init__(self, name):
         self.name = name
 
-Index('ix_debian_packages_name', DebianPackage.name)
-
 class DebianMaintainer(Base):
     __tablename__ = "debian_maintainers"
     
-    email = Column(String, primary_key=True)
-    name = Column('name', String)
+    email = Column(String, primary_key=True, index=True)
+    name = Column(String, index=True)
+    mappings = relationship("DebianPackagePeopleMapping", backref="maintainer")
     
     def __init__(self, email, name):
         self.email = email
         self.name = name
 
-Index('ix_debian_maintainers_name', DebianMaintainer.name)
-
 class DebianPackagePeopleMapping(Base):
     __tablename__ = "debian_package_people_mapping"
     
-    debian_package_name = Column(
+    package_name = Column(
         String, ForeignKey('debian_packages.name'), primary_key=True)
-    debian_maintainer_email = Column(
-        String, ForeignKey('debian_maintainers.email'), primary_key=True)
+    maintainer_email = Column(
+        String, ForeignKey('debian_maintainers.email'),
+        primary_key=True, index=True)
     
-    def __init__(self, debian_package_name, debian_maintainer_email):
-        self.debian_package_name = debian_package_name
-        self.debian_maintainer_email = debian_maintainer_email
-
-Index('ix_debian_package_people_mapping_maintainer_id',
-      DebianPackagePeopleMapping.debian_maintainer_email)
+    def __init__(self, package_name, maintainer_email):
+        self.package = package_name
+        self.maintainer = maintainer_email
