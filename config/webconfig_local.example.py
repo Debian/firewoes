e538a2f467045a74e81c9ecc47ea49d94b7f6b05
Matthieu Caneill <matthieu.caneill42@gmail.com>
2013-08-01 11:21:11 +0200

    fix: default config variables moved to webconfig_default

--- config/webconfig_local.example.py
+++ config/webconfig_local.example.py
@@ -3,15 +3,4 @@
 # database
 DATABASE_URI = "postgresql://matthieu:matthieu@localhost:5432/firewose"
 
-# the url pattern used to generate urls to point on source code
-DEBIAN_SOURCES_URL = "http://sources.debian.net/src/{package}/{version}-{release}/{path}?msg={message}&hl={lines_range}#L{anchor}"
-
-FEDORA_SOURCES_URL = "http://foo.bar"
-
-# the url pattern used for embedded source code
-DEBIAN_EMBEDDED_SOURCES_URL = "http://sources.debian.net/embedded/{package}/{version}-{release}/{path}?msg={message}&hl={lines_range}#L{anchor}"
-
-FEDORA_EMBEDDED_SOURCES_URL = "http://foo.bar"
-
-# where one can find the source code of the app
 GITWEB_URL = "http://git.upsilon.cc/?p=firewose.git"
--- firewoes/web/webconfig_default.py
+++ firewoes/web/webconfig_default.py
@@ -39,3 +39,16 @@ SEARCH_MENU_MAX_NUMBER_OF_CHARS = 28
 
 # The number of results to display (per default) on a search results page
 SEARCH_RESULTS_OFFSET = 10
+
+# the url pattern used to generate urls to point on source code
+DEBIAN_SOURCES_URL = "http://sources.debian.net/src/{package}/{version}-{release}/{path}?msg={message}&hl={lines_range}#L{anchor}"
+
+FEDORA_SOURCES_URL = "http://foo.bar"
+
+# the url pattern used for embedded source code
+DEBIAN_EMBEDDED_SOURCES_URL = "http://sources.debian.net/embedded/{package}/{version}-{release}/{path}?msg={message}&hl={lines_range}#L{anchor}"
+
+FEDORA_EMBEDDED_SOURCES_URL = "http://foo.bar"
+
+# where one can find the source code of the app
+GITWEB_URL = ""
