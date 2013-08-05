fd8d8389a9a690e288c7e27ffe0f78e8107ca01f
Matthieu Caneill <matthieu.caneill42@gmail.com>
2013-08-05 15:04:32 +0200

    replace: firewose -> firewoes in config/db name

--- config/webconfig_local.example.py
+++ config/webconfig_local.example.py
@@ -1,6 +1,6 @@
 # The settings defined here will override those defined in webconfig.py
 
 # database
-DATABASE_URI = "postgresql://matthieu:matthieu@localhost:5432/firewose"
+DATABASE_URI = "postgresql://matthieu:matthieu@localhost:5432/firewoes"
 
 GITWEB_URL = "http://git.upsilon.cc/?p=firewose.git"
--- config/webconfig_testing.example.py
+++ config/webconfig_testing.example.py
@@ -6,4 +6,4 @@ TESTING = True
 SQLALCHEMY_ECHO = False
 
 # you should use another database, as it will empty and fill it again
-DATABASE_URI = "postgresql://matthieu:matthieu@localhost:5432/firewose_test"
+DATABASE_URI = "postgresql://matthieu:matthieu@localhost:5432/firewoes_test"
